05a8deaabaa8eae20f5f960cd348dfe884ba5459
Sascha Arnold <sascha.arnold@dfki.de>
2015-05-18 18:01:22 +0200

    revised driver task

--- scripts/test.rb
+++ scripts/test.rb
@@ -2,24 +2,21 @@ require 'orocos'
 include Orocos
 
 if !ARGV[0]
-    STDERR.puts "usage: test.rb <device name> <baud rate>"
+    STDERR.puts "usage: test.rb 'serial://<device name>:<baud rate>'"
     exit 1
 end
 
 Orocos.initialize
 
-Orocos::Process.run 'digiquartz_pressure::Task' => "digiquartz_pressure" do
+Orocos.run 'digiquartz_pressure::Task' => "digiquartz_pressure" do
     driver = TaskContext.get 'digiquartz_pressure'
     #Orocos.log_all_ports
 
-    driver.device = ARGV[0]
-    driver.baudrate = Integer(ARGV[1])
+    driver.io_port = ARGV[0]
     driver.integration_time = 0.188
     driver.configure
     driver.start
 
-    loop do        
-	sleep 0.01
-    end
+    Orocos.watch driver
 end
 
